bdea9c020969e80e6eb685afa6de1e5f7ef059bc
Andy-Kin3 <49214978+Andy-Kin3@users.noreply.github.com>
2021-09-18 17:58:39 +0100

    Uploaded entire file pack
    uplaoded the whole project

---
+++ src/Services.js
@@ -0,0 +1,27 @@
+import React from 'react'
+// import { useStateValue } from './StateProvider'
+import "./Services.css"
+
+function Services(props) {
+    // const dispatch = useStateValue();
+    // const addService = () => {
+    //     dispatch({
+    //         item:{
+    //             id:props.id,
+    //             title:props.title,
+    //             image:props.image,
+    //         }
+    //     });
+    // };
+    return (
+        <div className="services">
+            <div className="service_info">
+                <p>{props.item}</p>
+            </div>
+            <img src={props.image} alt="service"/>
+            {/* <button onClick={addService}>MORE</button> */}
+        </div>
+    )
+}
+
+export default Services
